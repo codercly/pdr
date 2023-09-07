252021511bde2aa344d63d941e91a0ff0e29e9c7
codercly <rrclementirafael@gmail.com>
2023-09-07 15:03:59 -0400

    feat: add search products / search products bar

--- pdr-app/src/services/foodService.js
+++ pdr-app/src/services/foodService.js
@@ -2,3 +2,7 @@ import { sample_foods } from "../data";
 
 
 export const getAll = async () => sample_foods;
+
+export const search = async searchTerm => sample_foods.filter(item => 
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())    
+);
