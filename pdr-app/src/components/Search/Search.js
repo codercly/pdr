e9f5b22be42f1cb465b49200211f4ab450e2c1c8
codercly <rrclementirafael@gmail.com>
2023-09-10 16:52:50 -0400

    refactor: Fixing Search Issue

--- pdr-app/src/components/Search/Search.js
+++ pdr-app/src/components/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './search.module.css'
 import { useNavigate, useParams } from 'react-router-dom'
 
@@ -7,17 +7,21 @@ export default function Search() {
     const navigate = useNavigate()
     const { searchTerm } = useParams()
 
+    useEffect(() => {
+        setTerm(searchTerm ?? '')
+    }, [searchTerm])
+
     const search = async () => {
         term ? navigate('/search/' + term) : navigate('/')
     }
     return (
         <div className={classes.container}>
             <input
-                type='text' 
+                type='text'
                 placeholder='Pesquise por um produto'
                 onChange={e => setTerm(e.target.value)}
                 onKeyUp={e => e.key === 'Enter' && search()}
-                defaultValue={searchTerm}
+                value={term}
             />
             <button onClick={search}>Search</button>
         </div>
